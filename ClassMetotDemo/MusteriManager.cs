5b8702573e7db5165d1478313fb6f7fe7e30b1c6
Kutluhan Kemal YAZICIOĞLU <kutluhan.yazicioglu@ogr.sakarya.edu.tr>
2021-01-20 22:05:05 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri: " + musteri.Id+ " "+ musteri.Ad+" "+ musteri.Soyad+" "+ musteri.Adres +"Müşteri Bilgileri Alındı. " );
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Adres + "Müşteri Bilgileri Silindi." );
+        }
+
+        public void MusteriListesi(Musteri[] musteris)
+        {
+            foreach (var musteri in musteris)
+            {
+                Console.WriteLine("Müşteri Id:  " + musteri.Id);
+                Console.WriteLine("Müşteri Adı:  " + musteri.Ad);
+                Console.WriteLine("Müşteri Soyad:  " + musteri.Soyad);
+                Console.WriteLine("Müşteri Adres:  " + musteri.Adres);
+            }
+        
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1000;
+            musteri1.Ad = " Kemal ";
+            musteri1.Soyad = " Yazıcıoğlu ";
+            musteri1.Adres = " IST/Turkey ";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1000;
+            musteri2.Ad = " Kutluhan ";
+            musteri2.Soyad = " Yazıcıoğlu ";
+            musteri2.Adres = " IST/Turkey ";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1000;
+            musteri3.Ad = " Burak ";
+            musteri3.Soyad = " Yazıcıoğlu ";
+            musteri3.Adres = " IST/Turkey ";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 1000;
+            musteri4.Ad = " Burcu ";
+            musteri4.Soyad = " Yazıcıoğlu ";
+            musteri4.Adres = " IST/Turkey ";
+
+            Musteri musteri5 = new Musteri();
+            musteri5.Id = 1000;
+            musteri5.Ad = " İsmail ";
+            musteri5.Soyad = " Yazıcıoğlu ";
+            musteri5.Adres = " IST/Turkey ";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4, musteri5 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("\nMüşteri Ekle: \n");
+
+            foreach (var musteri in musteriler)
+            {
+                Console.Write("Müşteri Bilgileri: ");
+                musteriManager.MusteriEkle(musteri);
+            }
+
+
+
+            Console.WriteLine("\nMüşteri Sil: \n");
+
+            foreach (var musteri in musteriler)
+            {
+                Console.Write("Müşteri Bilgileri: ");
+                musteriManager.MusteriSil(musteri);
+            }
+
+            Console.WriteLine("\nMüşteri Listesi: \n");
+
+           
+            musteriManager.MusteriListesi(musteriler);
+
+
+
+        }
+    }
+}
